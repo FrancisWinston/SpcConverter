91252474f62ec988b5e2b7569e84cefec36753ee
ManhattanFlash <original.francis.winston@gmail.com>
2023-09-06 14:46:06 +0300

    Added basic class hierarchy.

---
+++ Domain/Models/KompasModels/Statement.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpcConverter.Domain.Models.KompasModels
+{
+    internal class Statement
+    {
+    }
+}

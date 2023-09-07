08b309fcab2dc7be12ad37ca6e9b039ebc591fdc
ManhattanFlash <original.francis.winston@gmail.com>
2023-09-07 11:07:17 +0300

    +Forms sizes & titles

--- Forms/AboutForm.Designer.cs
+++ Forms/AboutForm.Designer.cs
@@ -28,10 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "AboutForm";
+            SuspendLayout();
+            // 
+            // AboutForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(624, 441);
+            MaximumSize = new Size(640, 480);
+            MinimumSize = new Size(640, 480);
+            Name = "AboutForm";
+            Text = "СПО \"Конвертер\" - Справка";
+            ResumeLayout(false);
         }
 
         #endregion
--- Forms/SettingsForm.Designer.cs
+++ Forms/SettingsForm.Designer.cs
@@ -28,10 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "SettingsForm";
+            SuspendLayout();
+            // 
+            // SettingsForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(624, 441);
+            MaximumSize = new Size(640, 480);
+            MinimumSize = new Size(640, 480);
+            Name = "SettingsForm";
+            Text = "СПО \"Конвертер\" - Настройки";
+            ResumeLayout(false);
         }
 
         #endregion
